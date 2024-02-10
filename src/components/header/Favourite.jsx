d07916418ac544f4693670f128d9903b7a2b63d9
saifur111 <ssaaiiffuurr240@gmail.com>
2024-02-10 13:27:32 +0600

    fixed favourite bug

--- src/components/header/Favourite.jsx
+++ src/components/header/Favourite.jsx
@@ -1,9 +1,13 @@
+/* eslint-disable react/prop-types */
 import HeartIcon from "../../assets/heart.svg";
+import RedHeartIcon from "../../assets/heart-red.svg";
+import { useFavouriteData } from "../../provider/FavouriteProvider";
 
 function Favourite({ onShow }) {
+    const { favourites } = useFavouriteData();
     return (
         <div className="p-2 hover:bg-black/30 cursor-pointer flex gap-2 items-center rounded-md transition-all">
-            <img src={HeartIcon} alt="heart" />
+            <img src={favourites.length>0?RedHeartIcon:HeartIcon} alt="heart" />
             <span onClick={onShow}>Favourite Locations</span>
         </div>
     );
